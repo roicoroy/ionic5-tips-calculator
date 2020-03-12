5f18ae5561f52e9d7e0ca828a34b51ef80a77f5a
Ricardo Watanabe Bento <ricardo@flowhospitalitytraining.co.uk>
2020-03-12 19:57:17 +0000

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicSelectableModule } from 'ionic-selectable';
+import { WheelSelector } from '@ionic-native/wheel-selector/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -15,6 +16,7 @@ import { IonicSelectableModule } from 'ionic-selectable';
     IonicSelectableModule
   ],
   providers: [
+    WheelSelector,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/calculator/calculator.page.ts
+++ src/app/calculator/calculator.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { WheelSelector } from '@ionic-native/wheel-selector/ngx';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.page.html',
@@ -9,7 +10,9 @@ import { Router } from '@angular/router';
 export class CalculatorPage implements OnInit {
   waiters: any = [];
   constructor(
-    private router: Router
+    private router: Router,
+    private selector: WheelSelector,
+    public alertController: AlertController
   ) {
     this.waiters = [
       {
@@ -26,8 +29,73 @@ export class CalculatorPage implements OnInit {
       }
     ];
   }
-
   ngOnInit() {
+  }
+  async addHours() {
+    const alert = await this.alertController.create({
+      header: 'Checkbox',
+      inputs: [
+        {
+          type: 'checkbox',
+          label: '1',
+          value: 1
+        },
+        {
+          type: 'checkbox',
+          label: '2',
+          value: 2
+        },
+        {
+          type: 'checkbox',
+          label: '3',
+          value: 3
+        },
+        {
+          type: 'checkbox',
+          label: '4',
+          value: 4
+        },
+        {
+          type: 'checkbox',
+          label: '5',
+          value: 5
+        },
+        {
+          type: 'checkbox',
+          label: '6',
+          value: 6
+        },
+        {
+          type: 'checkbox',
+          label: '7',
+          value: 7
+        },
+        {
+          type: 'checkbox',
+          label: '8',
+          value: 8
+        },
+
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: (inputs) => {
+            console.log('Confirm Ok', inputs);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
   calculateEquation() {
     console.log('Calculated!');
